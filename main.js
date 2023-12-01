efd03326fc575a4c01f66b584e01dee5a8880726
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-01 13:53:12 +0500

    on work

--- main.js
+++ main.js
@@ -5,50 +5,66 @@ let datetime = document.getElementById("datettime");
 let description = document.getElementById("description");
 let taskkadiv = document.getElementById("taskkadiv");
 let done = document.getElementById("done")
-
+let day = new Date().toDateString();
 function currenttime() {
     displayClock();
     function displayClock() {
         var display = new Date().toLocaleTimeString();
-        timeshow.innerHTML = display;
+        timeshow.innerHTML = `${display}<br>${day} `;
         setTimeout(displayClock, 1000);
         return;
     }
     fulldiv.style.display = "none";
-    timeshow.innerHTML = `<div></div>`
     timeshow.style = `margin-top: 5%;
     font-size : 50px;
     color: #fff;
     text-align: center;
-box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-background-color: #ACD4D5;
-border: 1px solid black;
-height: 100px;
-width: 80%;
-margin-left: 10%;`
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+    background-color: #ACD4D5;
+    border: solid white;
+    border-radius: 15px;
+    height: auto;
+    width: 80%;
+    margin-left: 10%;`
+    tasks.style.display = "none"
 }
+
+let interval = false;
+let currentDateGetMilliSec;
+let alarmDateGetMillsec;
 
 function mytask() {
     fulldiv.style.display = "none";
     tasks.style.display = ` margin-top: 5%;
+    display:block;
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
   `
 }
-
 done.addEventListener("click", function () {
-    console.log(datetime.value)
-    taskkadiv.innerHTML = `
+    //     interval = true;
+    //     var alarmDate = new Date(datetime.value);
+    //     alarmDateGetMillsec = Math.round(alarmDate.getTime() / 1000)
+    //     // var equal = currentDateGetMilliSec - alarmDateGetMillsec;
+    //     // console.log(equal)
+    //     console.log("")
+    //     console.log(description.value)
+    //     console.log(datetime.value)
+    taskkadiv.innerHTML += `
     <div class="first" style ="height:auto;">
-                <div class="picone">
-                    <h3>${datetime.value}</h3>
-                </div>
-                <div class="h2">
-                    <h3>${description.value}</h3>
-                </div>
+    <h3 style ="color:#fff; padding-left: 10px;">${description.value} , ${datetime.value.slice(0, 10)}</h3>
+  <h3 style ="color:#fff;  padding-left: 70px; ">${datetime.value.slice(11)}</h3>
             </div>`
-
     fulldiv.style.display = "none";
+    description.value = "";
+    datetime.value = "";
+
 
 })
+
+function namazdiv() {
+    fulldiv.style.display = "block";
+    tasks.style.display = "none";
+    timeshow.style.display = "none";
+}
